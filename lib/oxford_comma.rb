68a17b2f412cc6df9b1f799a71593fae4ee13a83
Victoria Rathsmill <vlrathsmill@gmail.com>
2017-01-12 21:47:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+  if array.length == 1
+    return array.join(", ")
 
+  elsif array.length == 2
+    return array.join(" and ")
+
+  elsif array.length >= 3
+    result = "#{array[0...-1].join(", ")}, and #{array.last}"
+    return result
+  end
 end
